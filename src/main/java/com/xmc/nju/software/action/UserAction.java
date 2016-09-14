6feeb07bb48487bef123f2c0906e421c834597f8
xmc1993 <1170143845@qq.com>
2016-09-14 17:21:37 +0800

    test

--- src/main/java/com/xmc/nju/software/action/UserAction.java
+++ src/main/java/com/xmc/nju/software/action/UserAction.java
@@ -1,5 +1,6 @@
 package com.xmc.nju.software.action;
 
+import com.xmc.nju.software.entity.User;
 import com.xmc.nju.software.service.UserService;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class UserAction extends BaseAction {
     private String userName;
     private String password;
+    private User user;
 
     @Autowired
     private UserService userService;
 
     public void login(){
+        String formData = getFormData();
+        System.out.println(formData);
         System.out.println("over");
     }
 
